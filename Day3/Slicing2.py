669a67537735de813cfda8c1b46cbd5906f6f323
shadiaislamankur <shadiaislam469@gmail.com>
2024-03-24 20:58:34 +0600

    added new changes

---
+++ Day3/Slicing2.py
@@ -0,0 +1,18 @@
+my_list = ['apple', 'banana', 'cherry', 'date']
+print(my_list[0])
+print(my_list[1])
+print(my_list[1:3])
+print(my_list[:2])
+print(my_list[2:]) 
+sentence = "The quick brown fox jumps over the lazy dog"
+first_word = sentence[:3]
+print(first_word)
+second_word = sentence[4:9]
+third_word = sentence[10:15]
+print(second_word)
+print(third_word)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+odd_numbers = numbers[::2]
+print(odd_numbers) 
+even_numbers = numbers[1::2]
+print(even_numbers)
