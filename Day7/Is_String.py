2245107e11f318597df65a9c52f651e53cc273ec
shadiaislamankur <shadiaislam469@gmail.com>
2024-04-05 23:35:57 +0600

    7th day

---
+++ Day7/Is_String.py
@@ -0,0 +1,10 @@
+def new_string(text):
+    if len(text) >=2 and text[:2] == "Is":
+       return text
+    
+    else:
+        return "Is"+text
+    
+print(new_string("Array"))
+print(new_string("IsEmpty"))
+    
---
+++ Day7/sum_thrice.py
@@ -0,0 +1,10 @@
+def sum_thric(x,y,z):
+       sum=x+y+z
+
+       if x==y==z:
+         sum=sum*3
+       return sum
+
+
+print(sum_thric(1,2,3))
+print(sum_thric(3,3,3))
